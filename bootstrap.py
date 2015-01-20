4d875d740b6ba1abc4bcdf4f34bd6959ef49b21a
mriehl <max@riehl.io>
2015-01-20 13:49:37 +0100

    add license to script

--- bootstrap.py
+++ bootstrap.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
-
+#  Copyright (c) 2014-2015 Maximilien Riehl <max@riehl.io>
+#  This work is free. You can redistribute it and/or modify it under the
+#  terms of the Do What The Fuck You Want To Public License, Version 2,
+#  as published by Sam Hocevar. See the COPYING.wtfpl file for more details.
+#
 import sys
 sys.path.insert(0, "src/main/python")
 
