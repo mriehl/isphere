89c3c5f64d2c1fee4f6fc0d332bb424f294c4775
mriehl <max@riehl.io>
2015-01-22 16:46:57 +0100

    add some code examples

--- src/main/python/isphere/__init__.py
+++ src/main/python/isphere/__init__.py
@@ -5,7 +5,7 @@
 #
 
 """
-The isphere package.
+# The isphere package.
 
 - `isphere.command`: a subpackage defining REPL commands.
 - `isphere.cli`: a module for command line entrypoints.
@@ -14,4 +14,24 @@ _ `isphere.interactive_wrapper`: A `pyVmomi` API wrapper to make most things
 - `isphere.connection`: A connection and caching abstraction over
   `isphere.interactive_wrapper`.
 - `isphere.input`: a module for user input capabilities.
+
+
+## Full retrieval API quickstart
+
+    from isphere.interactive_wrapper import VVC
+    vvc = VVC(hostname)
+    vvc.connect(username, password)
+
+    for vm in vvc.get_all_vms():
+        print("{vm_name} on {host_system}".format(vm_name=vm.name, host_system=vm.runtime.host.name))
+
+## Selective retrieval API (faster)
+
+    from isphere.interactive_wrapper import VVC
+    vvc = VVC(hostname)
+    vvc.connect(username, password)
+
+    for vm in vvc.get_restricted_view_on_vms(["name", "runtime.host.name"]):
+        print("{vm_name} on {host_system}".format(vm_name=vm.name, host_system=vm.runtime.host.name))
+
 """
