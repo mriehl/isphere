6b61b509f1c653b3837335e1aeb6c7d629351242
mriehl <max@riehl.io>
2015-01-22 09:55:57 +0100

    adding toplevel docstring

--- src/main/python/isphere/__init__.py
+++ src/main/python/isphere/__init__.py
@@ -3,3 +3,15 @@
 #  terms of the Do What The Fuck You Want To Public License, Version 2,
 #  as published by Sam Hocevar. See the COPYING.wtfpl file for more details.
 #
+
+"""
+The isphere package.
+
+- `isphere.command`: a subpackage defining REPL commands.
+- `isphere.cli`: a module for command line entrypoints.
+_ `isphere.interactive_wrapper`: A `pyVmomi` API wrapper to make most things
+  dead simple.
+- `isphere.connection`: A connection and caching abstraction over
+  `isphere.interactive_wrapper`.
+- `isphere.input`: a module for user input capabilities.
+"""
