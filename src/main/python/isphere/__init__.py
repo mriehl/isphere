81046551834c570f453f05cb1f2912357a6f394b
Maximilien Riehl <max@riehl.io>
2015-01-23 22:31:26 +0100

    s/_/-/ to fix markdown list

--- src/main/python/isphere/__init__.py
+++ src/main/python/isphere/__init__.py
@@ -9,7 +9,7 @@
 
 - `isphere.command`: a subpackage defining REPL commands.
 - `isphere.cli`: a module for command line entrypoints.
-_ `isphere.interactive_wrapper`: A `pyVmomi` API wrapper to make most things
+- `isphere.interactive_wrapper`: A `pyVmomi` API wrapper to make most things
   dead simple.
 - `isphere.connection`: A connection and caching abstraction over
   `isphere.interactive_wrapper`.
