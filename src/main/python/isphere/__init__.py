ae60f1aa8b757e0f9ef85cd0700dacf366b4be4e
mriehl <max@riehl.io>
2015-02-18 16:10:00 +0100

    improve entrypoint documentation

--- src/main/python/isphere/__init__.py
+++ src/main/python/isphere/__init__.py
@@ -5,7 +5,14 @@
 #
 
 """
-# The isphere package.
+# isphere
+
+isphere (read: *interactive vSphere*) is a platform independent (mac, windows, linux)
+[**REPL**](https://en.wikipedia.org/wiki/Read%E2%80%93eval%E2%80%93print_loop)
+thanks to the [pyVmomi](https://pypi.python.org/pypi/pyvmomi)
+library, usable from your favourite terminal.
+It also features an easy to use **API** for programmatic access.
+
 
 - `isphere.command`: a subpackage defining REPL commands.
 - `isphere.cli`: a module for command line entrypoints.
@@ -16,7 +23,13 @@
 - `isphere.input`: a module for user input capabilities.
 
 
+# API capabilities
+
 ## Full retrieval API quickstart
+
+The full retrieval API makes it easy to get all items of a certain type.
+It will fully populate the items, which may take some time.
+See `isphere.interactive_wrapper.VVC` for all the possibilities.
 
     from isphere.interactive_wrapper import VVC
     vvc = VVC(hostname)
@@ -27,6 +40,10 @@
 
 ## Selective retrieval API (faster)
 
+The selective retrieval API is very fast but requires you to know ahead of time
+which attribute you want to access on the API items.
+See `isphere.interactive_wrapper.VVC` for all the possibilities.
+
     from isphere.interactive_wrapper import VVC
     vvc = VVC(hostname)
     vvc.connect(username, password)
@@ -34,4 +51,18 @@
     for vm in vvc.get_restricted_view_on_vms(["name", "runtime.host.name"]):
         print("{vm_name} on {host_system}".format(vm_name=vm.name, host_system=vm.runtime.host.name))
 
+## Caching API
+
+The caching API allows to access all available item names with an optional
+retrieval of the full item. It caches everything very aggressively and is
+especially useful if you need all the item names to decide which ones you want
+to use (for example regex matching).
+
+    >>> from isphere.connection import CachingVSphere
+    >>> vsphere_cache = CachingVSphere()
+    >>> vsphere_cache.fill() # rerun if you need to update the cache later
+    >>> all_vms = vsphere_cache.list_cached_vms()
+    >>> all_vms[0]
+    'some-vm-name'
+    >>> actual_vm = vsphere_cache.retrieve_vm(all_vms[0])
 """
